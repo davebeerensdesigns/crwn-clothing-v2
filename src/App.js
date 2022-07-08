596a8c4e7feb2f30144a88bfbd870bf4e2c2eaca
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2022-07-08 16:08:28 +0200

    category item component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,10 @@
-import './App.css';
-import './categories.styles.scss';
+import CategoryGridComponent from "./components/category-grid/category-grid.component";
+
 
 const App = () => {
 
-    const categories = [
-        {
-            id: 1,
-            title: 'Hats'
-        },
-        {
-            id: 2,
-            title: 'Jackets'
-        },
-        {
-            id: 3,
-            title: 'Sneakers'
-        },
-        {
-            id: 4,
-            title: 'Womens'
-        },
-        {
-            id: 5,
-            title: 'Mens'
-        }
-    ]
-
     return (
-        <div className='categories-container'>
-            {categories.map(({title, id}) => (
-                <div key={id} className='category-container'>
-                    <div className='background-image' />
-                    <div className='category-body-container'>
-                        <h2>{title}</h2>
-                        <p>Shop Now</p>
-                    </div>
-                </div>
-            ))}
-        </div>
+        <CategoryGridComponent/>
     )
 }
 
