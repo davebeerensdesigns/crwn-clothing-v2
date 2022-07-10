1b46487c3bd7eec8dc753af29f86d139a973cd9b
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2022-07-10 10:02:17 +0200

    styling for nav and logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import {Routes, Route} from "react-router-dom";
 import HomeComponent from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
 
 const App = () => {
+
+
+
+    const Shop = () => {
+        return (
+            <h1>I am the shop page</h1>
+        )
+    }
 
     return (
         <Routes>
             <Route path='/'
-                   element={
-                       <HomeComponent/>
-                   }/>
+                   element={<Navigation/>}>
+                <Route index path='/'
+                       element={
+                           <HomeComponent/>
+                       }/>
+                <Route path='shop'
+                       element={
+                           <Shop/>
+                       }/>
+            </Route>
         </Routes>
     )
 }
