a9f05a88d96e5898a2741a35db25e6367df49e5b
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2022-07-12 14:58:51 +0200

    Get Products + Categories From Firestore

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -9,7 +9,16 @@ import {
     signOut,
     onAuthStateChanged,
 } from 'firebase/auth';
-import {getFirestore, doc, getDoc, setDoc, collection, writeBatch} from 'firebase/firestore';
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc,
+    collection,
+    writeBatch,
+    query,
+    getDocs
+} from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDSeD5kA6A8jwmN03U58CDTZLHqnZfHnI8",
@@ -47,6 +56,22 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
 
     await batch.commit()
     console.log('done')
+}
+
+export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'categories');
+
+    const q = query(collectionRef);
+
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+        const { title, items } = docSnapshot.data();
+        acc[title.toLowerCase()] = items;
+        return acc;
+    }, {});
+
+    return categoryMap;
+
 }
 
 export const createUserDocumentFromAuth = async (
