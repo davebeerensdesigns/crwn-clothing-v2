68385b297ace89428013cdf15a9e3e93d191fe1f
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2022-07-12 15:33:27 +0200

    Category Preview Component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,26 +1,20 @@
 import {Fragment, useContext} from "react";
 
 import {CategoriesContext} from "../../contexts/categories.context";
-import ProductCardComponent from "../../components/product-card/products-card.component";
 
 import './shop.styles.scss';
+import CategoryPreviewComponent from "../../components/category-preview/category-preview.component";
 
 const ShopComponent = () => {
     const {categoriesMap} = useContext(CategoriesContext)
     return (
-        <Fragment>
-            {Object.keys(categoriesMap).map((title) => (
-                <Fragment key={title}>
-                    <h2>{title}</h2>
-                    <div className='products-container'>
-                        {categoriesMap[title].map((product) => (
-                            <ProductCardComponent key={product.id}
-                                                  product={product}/>
-                        ))}
-                    </div>
-                </Fragment>
-            ))}
-        </Fragment>
+        <div className='shop-container'>
+            {Object.keys(categoriesMap).map((title) => {
+                    const products = categoriesMap[title]
+                return <CategoryPreviewComponent key={title} title={title} products={products}/>
+                })
+            }
+        </div>
     );
 };
 
