4b76ab75fc50c39fc63d95b42fb4be1217a412df
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2022-07-12 09:55:03 +0200

    product card component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,12 +1,16 @@
-import SHOP_DATA from '../../shop-data.json';
+import { useContext} from "react";
+
+import { ProductsContext } from "../../contexts/products.context";
+import ProductCardComponent from "../../components/product-card/products-card.component";
+
+import './shop.styles.scss';
 
 const ShopComponent = () => {
+    const {products} = useContext(ProductsContext)
     return (
-        <div>
-            {SHOP_DATA.map(({id, name}) => (
-                <div key={id}>
-                    <h1>{name}</h1>
-                </div>
+        <div className='products-container'>
+            {products.map((product) => (
+                <ProductCardComponent key={product.id} product={product} />
             ))}
         </div>
     );
