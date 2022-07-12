bfc8ba325788963f3d714d71ef8550aca7084810
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2022-07-12 15:54:08 +0200

    Category Page

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,6 +1,7 @@
 import {Routes, Route} from 'react-router-dom';
 
 import CategoriesPreviewComponent from "../categories-preview/categories-preview.component";
+import CategoryComponent from "../category/category.component";
 
 import './shop.styles.scss';
 
@@ -8,6 +9,7 @@ const ShopComponent = () => {
     return (
         <Routes>
             <Route index element={<CategoriesPreviewComponent />} />
+            <Route path=':category' element={<CategoryComponent />} />
         </Routes>
     );
 };
