3cbfbba75f2d4b676dcf794d8f6f00efd9e84b0d
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2022-07-14 13:49:42 +0200

    Demystifying Middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,25 @@
 import {compose, createStore, applyMiddleware} from "redux";
-import {logger} from "redux-logger/src";
+// import {logger} from "redux-logger/src";
 
 import {rootReducer} from "./root-reducer";
 
-const middlewares = [logger];
+const loggerMiddleware = (store) => (next) => (action) => {
+    if(!action.type){
+        return next(action);
+    }
+
+    console.log('type:', action.type);
+    console.log('payload:', action.payload);
+    console.log('currentState:', store.getState());
+
+    next(action);
+
+    console.log('newState:', store.getState());
+}
+
+const middlewares = [loggerMiddleware];
+
+// const middlewares = [logger];
 
 const composeEnhancers = compose(applyMiddleware(...middlewares));
 
