a7623fad79689df012bcc38914119b90ff0a2efc
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2022-07-14 12:41:52 +0200

    Categories Selectors

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import reportWebVitals from './reportWebVitals';
 
 import {store} from "./store/store";
 
-import {CategoriesProvider} from "./contexts/categories.context";
-
 import {CartProvider} from "./contexts/cart.context";
 
 import App from './App';
@@ -18,11 +16,9 @@ root.render(
     <React.StrictMode>
         <Provider store={store}>
             <BrowserRouter>
-                <CategoriesProvider>
-                    <CartProvider>
-                        <App/>
-                    </CartProvider>
-                </CategoriesProvider>
+                <CartProvider>
+                    <App/>
+                </CartProvider>
             </BrowserRouter>
         </Provider>
     </React.StrictMode>
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,9 +1,25 @@
+import {useEffect} from "react";
 import {Routes, Route} from 'react-router-dom';
+import {useDispatch} from "react-redux";
+
+import {getCategoriesAndDocuments} from "../../utils/firebase/firebase.utils";
+import {setCategoriesMap} from "../../store/categories/category.action";
 
 import CategoriesPreviewComponent from "../categories-preview/categories-preview.component";
 import CategoryComponent from "../category/category.component";
 
 const ShopComponent = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments('categories');
+            dispatch(setCategoriesMap(categoryMap));
+        }
+        getCategoriesMap();
+    }, [])
+
     return (
         <Routes>
             <Route index element={<CategoriesPreviewComponent />} />
