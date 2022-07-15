1adb5271253e904bb8591632aa585a9809f3c1cf
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2022-07-15 09:30:28 +0200

    Migrating Cart Context to Redux Pt. 1

---
+++ src/store/cart/cart.action.js
@@ -0,0 +1,48 @@
+import {CART_ACTIONS_TYPE} from "./cart.types";
+import {createAction} from "../../utils/reducer/reducer.utils";
+
+const addCartItem = (cartItems, productToAdd) => {
+    const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id);
+
+    if (existingCartItem) {
+        return cartItems.map((cartItem) => cartItem.id === productToAdd.id
+            ? {...cartItem, quantity: cartItem.quantity + 1}
+            : cartItem)
+    }
+
+    return [...cartItems, {...productToAdd, quantity: 1}];
+}
+
+const removeCartItem = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find((cartItem) => cartItem.id === cartItemToRemove.id);
+
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+    }
+
+    return cartItems.map((cartItem) => cartItem.id === cartItemToRemove.id
+        ? {...cartItem, quantity: cartItem.quantity - 1}
+        : cartItem)
+
+}
+
+const clearCartItem = (cartItems, cartItemToClear) => cartItems.filter(cartItem => cartItem.id !== cartItemToClear.id)
+
+const addItemToCart = (productToAdd) => {
+    const newCartItems = addCartItem(cartItems, productToAdd)
+    updateCartItemsReducer(newCartItems)
+}
+
+const removeItemFromCart = (cartItemToRemove) => {
+    const newCartItems = removeCartItem(cartItems, cartItemToRemove)
+    updateCartItemsReducer(newCartItems)
+}
+
+const clearItemFromCart = (cartItemToClear) => {
+    const newCartItems = clearCartItem(cartItems, cartItemToClear)
+    updateCartItemsReducer(newCartItems)
+}
+
+
+export const setIsCartOpen = (bool) =>
+    createAction(CART_ACTIONS_TYPE.SET_IS_CART_OPEN, bool);
--- src/store/store.js
+++ src/store/store.js
@@ -3,19 +3,19 @@ import {logger} from "redux-logger/src";
 
 import {rootReducer} from "./root-reducer";
 
-const loggerMiddleware = (store) => (next) => (action) => {
-    if(!action.type){
-        return next(action);
-    }
-
-    console.log('type:', action.type);
-    console.log('payload:', action.payload);
-    console.log('currentState:', store.getState());
-
-    next(action);
-
-    console.log('newState:', store.getState());
-}
+// const loggerMiddleware = (store) => (next) => (action) => {
+//     if(!action.type){
+//         return next(action);
+//     }
+//
+//     console.log('type:', action.type);
+//     console.log('payload:', action.payload);
+//     console.log('currentState:', store.getState());
+//
+//     next(action);
+//
+//     console.log('newState:', store.getState());
+// }
 
 // const middlewares = [loggerMiddleware];
 
