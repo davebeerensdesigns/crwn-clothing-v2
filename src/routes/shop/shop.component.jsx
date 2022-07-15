10278020793293fbdc34997733ab6445c48b943d
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2022-07-15 12:51:52 +0200

    Redux-Thunk Pt. 2

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,8 +2,7 @@ import {useEffect} from "react";
 import {Routes, Route} from 'react-router-dom';
 import {useDispatch} from "react-redux";
 
-import {getCategoriesAndDocuments} from "../../utils/firebase/firebase.utils";
-import {setCategories} from "../../store/categories/category.action";
+import {fetchCategoriesAsync} from "../../store/categories/category.action";
 
 import CategoriesPreviewComponent from "../categories-preview/categories-preview.component";
 import CategoryComponent from "../category/category.component";
@@ -13,11 +12,7 @@ const ShopComponent = () => {
     const dispatch = useDispatch();
 
     useEffect(()=>{
-        const getCategoriesMap = async () => {
-            const categoriesArray = await getCategoriesAndDocuments('categories');
-            dispatch(setCategories(categoriesArray));
-        }
-        getCategoriesMap();
+        dispatch(fetchCategoriesAsync());
     }, [])
 
     return (
