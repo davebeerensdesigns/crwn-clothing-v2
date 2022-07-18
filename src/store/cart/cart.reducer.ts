cfde08c333fe7d5a2a68e0c4788e7d24ae1895d7
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2022-07-18 17:12:36 +0200

    Typing Redux Cart Files Pt. 2

---
+++ src/store/cart/cart.reducer.ts
@@ -0,0 +1,38 @@
+import {
+  setCartItems,
+  setIsCartOpen,
+} from './cart.action'
+
+import { AnyAction } from 'redux'
+
+import {
+  CartItem,
+} from './cart.types'
+
+export type CartState = {
+  isCartOpen: boolean;
+  cartItems: CartItem[];
+}
+
+const CART_INITIAL_STATE = {
+  isCartOpen: false,
+  cartItems: [],
+}
+
+export const cartReducer = (
+  state = CART_INITIAL_STATE, action: AnyAction): CartState => {
+
+  if (setIsCartOpen.match(action)) {
+    return {
+      ...state,
+      isCartOpen: action.payload,
+    }
+  }
+  if (setCartItems.match(action)) {
+    return {
+      ...state,
+      cartItems: action.payload,
+    }
+  }
+  return state
+}
