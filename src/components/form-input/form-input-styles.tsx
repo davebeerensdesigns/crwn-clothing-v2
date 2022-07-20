ac7c55e191529dc56b788342de870ee380658b67
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2022-07-20 11:33:35 +0200

    Typing Input & Form Components

--- src/components/form-input/form-input-styles.tsx
+++ src/components/form-input/form-input-styles.tsx
@@ -17,8 +17,11 @@ export const Group = styled.div`
     letter-spacing: 0.3em;
   }
 `
+type FormInputLabelProps = {
+    shrink?: boolean;
+}
 
-export const FormInputLabel = styled.label`
+export const FormInputLabel = styled.label<FormInputLabelProps>`
   color: ${subColor};
   font-size: 16px;
   font-weight: normal;
