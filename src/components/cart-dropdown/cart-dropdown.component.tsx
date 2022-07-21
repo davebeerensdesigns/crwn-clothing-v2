988f35f700168a42b8c2143ecde67f186d613e94
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2022-07-21 09:41:37 +0200

    Hooks: useCallback Pt. 1

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -1,3 +1,5 @@
+import {useCallback} from "react";
+
 import {useSelector} from "react-redux";
 import {useNavigate} from 'react-router-dom';
 
@@ -11,9 +13,10 @@ const CartDropdownComponent = () => {
     const cartItems = useSelector(selectCartItems)
     const navigate = useNavigate();
 
-    const goToCheckoutHandler = () => {
+    const goToCheckoutHandler = useCallback(() => {
         navigate('/checkout')
-    }
+    }, []);
+    
     return (
         <CartDropdownContainer>
             <CartItems>
